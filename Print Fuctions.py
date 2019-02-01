f4766a0fbdc3f7b90135bb10c2539014c505f7d5
Ayaz Ahmed <Ayazsoomro13@gmail.com>
2019-02-01 21:23:36 +0500

    1st programm in python by using Print Function

---
+++ Print Fuctions.py
@@ -0,0 +1,13 @@
+print (5 / 2)
+# this will trank the .5 and the output will be only 2
+print (5 // 2)
+
+ # ** is used to power it means 2 power 3 in python will be
+
+print(2**2)
+
+
+ # print function is differently used in different condition
+print('Ayaz')
+
+ 
