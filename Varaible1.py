e39da9a25fbc2d94dcd62fbdeb4647415bf6135e
Ayaz Ahmed <Ayazsoomro13@gmail.com>
2019-02-01 23:12:20 +0500

    this is for Varable detail

---
+++ Varaible1.py
@@ -0,0 +1,3 @@
+name = 'Ayaz'
+
+len(name)
---
+++ Varaible2.py
@@ -0,0 +1,20 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name= "youtube"
+>>> len(name)
+7
+>>> name[0]
+'y'
+>>> name[0:3]
+'you'
+>>> name[1:7]
+'outube'
+>>> name[0:]
+'youtube'
+>>> name[1:10]
+'outube'
+>>> name[2:10]
+'utube'
+>>> name[:5]
+'youtu'
+>>> 
